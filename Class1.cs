c5fb7c30b8bc818d562a9a71840e34db68965a5a
porkodi-j <j.porkodi@gmail.com>
2023-12-19 13:59:45 +0000

    First commit

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    //Class Library project for Math Operations
+    public class MathOperations
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public bool FindOddEven(int num1)
+        {
+            if (num1 % 2 == 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
